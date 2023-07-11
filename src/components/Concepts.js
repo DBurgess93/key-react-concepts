2979cf7e7de6a8dd07d9bfcb418c139d32e3020f
Daniel Burgess <dan-burgess@hotmail.com>
2023-07-11 23:45:53 +0930

    Out put Key Concepts data and create Concepts and Concept components

---
+++ src/components/Concepts.js
@@ -0,0 +1,13 @@
+import Concept from './Concept'
+
+const Concepts = ({ concepts }) => {
+  return (
+    <ul id="concepts">
+      <Concept concept={concepts[0]} />
+      <Concept concept={concepts[1]} />
+      <Concept concept={concepts[2]} />
+    </ul>
+  )
+}
+
+export default Concepts
